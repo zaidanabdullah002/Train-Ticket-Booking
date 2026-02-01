4f4796929d4ae7088157fb093e6becf5cc89c10f
abdullahzaidan <zaidanabdullah002@gmail.com>
2026-02-01 12:07:37 +0530

    Revert "Train Ticket Booking System(BACKEND)"
    This reverts commit 551cc9b93a823423587d0eaa5de07803fbabefc4.

--- src/main/java/org/data/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package org.data;
-
-import org.entities.SeatAvailability;
-
-public interface SeatRepository {
-    SeatAvailability find(int TrainNo, String date);
-    void save(SeatAvailability seatAvailability);
-    SeatAvailability showSeats(int TrainNo,String date);
-}
